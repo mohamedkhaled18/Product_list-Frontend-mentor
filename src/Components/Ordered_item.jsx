a96bdeafb41dfb295f5183eae1d5aff9813422f6
mohamedkhaled18 <mohamedkhaledabrahim@gmail.com>
2025-07-20 16:16:47 +0200

    Edit images path

--- src/Components/Ordered_item.jsx
+++ src/Components/Ordered_item.jsx
@@ -4,11 +4,11 @@ const OrderedItem = ({ orderedCount, handleDecrement, handleIncrement }) => {
   return (
     <div className="quantity w-[50%] m-auto text-white font-semibold flex items-center justify-between gap-2 bg-[var(--Red)] p-2 rounded-[999px] relative bottom-[20px]">
       <button onClick={handleDecrement} className="decrement-btn">
-        <img className='w-[15px] h-[15px]' src="../../public/assets/icon/icon-decrement-quantity.svg" alt="" />
+        <img className='w-[15px] h-[15px]' src="../assets/icon/icon-decrement-quantity.svg" alt="" />
       </button>
       <span>{ orderedCount }</span>
       <button onClick={handleIncrement}  className='increment-btn'>
-        <img className='w-[15px] h-[15px]' src="../../public/assets/icon/icon-increment-quantity.svg" alt="" />
+        <img className='w-[15px] h-[15px]' src="../assets/icon/icon-increment-quantity.svg" alt="" />
       </button>
     </div>
   )
